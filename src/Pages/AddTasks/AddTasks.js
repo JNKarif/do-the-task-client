5ce8e21d8d5aa4da07d582a36c7d5b67908b7bd6
JNKarif <arif.rahman041@gmail.com>
2022-12-30 21:24:44 +0600

    my-tasks

--- src/Pages/AddTasks/AddTasks.js
+++ src/Pages/AddTasks/AddTasks.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const AddTasks = () => {
+
+const navigate= useNavigate()
+
+const handleSubmit = (event)=>{
+    event.preventDefault()
+    const form= event.target;
+    const task = form.task.value;
+    const picture = form.picture.files[0];  // get the first file from the list of files
+  
+    const formData = new FormData();  // create a new FormData object
+    formData.append('task', task);  // add the task field to the FormData object
+    formData.append('picture', picture);  // add the picture file to the FormData object
+  
+    fetch('http://localhost:5000/taskslist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data'  // set the correct Content-Type header
+      },
+      body: formData  // pass the FormData object directly as the body
+    })
+    .then(res=> res.json())
+    .then(data=> {
+      console.log(data)
+      if (data.acknowledged) {
+          
+        toast.success(`${task} added successfully`)
+        navigate('/myTasks')
+      }
+    })
+  }
+  
     return (
         <div className='text-center mx-auto w-1/2'>
             <p className='text-3xl my-3'>Add Your Tasks</p>
-            <div className="control mb-3" >
-                <input className="input" type="text" placeholder="Text input" />
+            <form onSubmit={handleSubmit} className="control mb-3" >
+                <input className="input" type="text" placeholder="Text input" name='task' />
 
                 <div class="file has-name" className='mt-3'>
                     <label class="file-label">
-                        <input class="file-input" type="file" name="resume" />
+                        <input class="file-input" type="file" name="picture" />
                         <span class="file-cta">
                             <span class="file-icon">
                                 <i class="fas fa-upload"></i>
@@ -27,7 +60,7 @@ const AddTasks = () => {
                     <input class="button" type="reset" value="Reset"></input>
                 </div>
 
-            </div>
+            </form>
         </div>
     );
 };
--- src/Pages/MyTasks/MyTasks.js
+++ src/Pages/MyTasks/MyTasks.js
@@ -1,110 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MyTasks = () => {
+
+const [tasks, setTasks]=useState([]);
+
+useEffect(()=>{
+  fetch(`http://localhost:5000/taskslist`)
+  .then(res=>res.json())
+  .then(data=> setTasks(data))
+  .catch(err=>console.error(err))
+}
+,[])
+
+
+// useEffect(()=>{
+  //   fetch(`http://localhost:5000/users/buyer/buyer`)
+  //   .then(res=>res.json())
+  //   .then(data=>setBuyers(data))
+  //   .catch(err=>console.error(err))
+  // },[])
     return (
-        <div>
+        <div class='is-centered'>
+          <div class='is-half'>
+            
+          
             <table class="table is-centered">
   <thead>
     <tr>
-      <th><abbr title="Position">Pos</abbr></th>
-      <th>Team</th>
-      <th><abbr title="Played">Pld</abbr></th>
-      <th><abbr title="Won">W</abbr></th>
-      <th><abbr title="Drawn">D</abbr></th>
-      <th><abbr title="Lost">L</abbr></th>
-      <th><abbr title="Goals for">GF</abbr></th>
-      <th><abbr title="Goals against">GA</abbr></th>
-      <th><abbr title="Goal difference">GD</abbr></th>
-      <th><abbr title="Points">Pts</abbr></th>
-      <th>Qualification or relegation</th>
+      <th><abbr title="Position">No.</abbr></th>
+      <th>Task Name</th>
+      <th><abbr title="Played">Task Picture</abbr></th>
+      <th><abbr title="Played">Action</abbr></th>
+      
     </tr>
   </thead>
   <tfoot>
-    <tr>
-      <th><abbr title="Position">Pos</abbr></th>
-      <th>Team</th>
-      <th><abbr title="Played">Pld</abbr></th>
-      <th><abbr title="Won">W</abbr></th>
-      <th><abbr title="Drawn">D</abbr></th>
-      <th><abbr title="Lost">L</abbr></th>
-      <th><abbr title="Goals for">GF</abbr></th>
-      <th><abbr title="Goals against">GA</abbr></th>
-      <th><abbr title="Goal difference">GD</abbr></th>
-      <th><abbr title="Points">Pts</abbr></th>
-      <th>Qualification or relegation</th>
+
+{
+  tasks && 
+  <> {
+    tasks.map((task, i)=>
+      <tr key={i}>
+      <th><abbr title="Position">{i+1}</abbr></th>
+      <th>{task.task}</th>
+      <th><abbr title="Played">{task.picture}</abbr></th>
+      <th><abbr title="Played">
+      <button class="button is-success">Done</button>
+      <button class="button is-danger">Will do</button>
+        </abbr></th>
     </tr>
+    )
+  }</>
+ 
+}
+    
+   
   </tfoot>
   <tbody>
-    <tr>
-      <th>1</th>
-      <td><a href="https://en.wikipedia.org/wiki/Leicester_City_F.C." title="Leicester City F.C.">Leicester City</a> <strong>(C)</strong>
-      </td>
-      <td>38</td>
-      <td>23</td>
-      <td>12</td>
-      <td>3</td>
-      <td>68</td>
-      <td>36</td>
-      <td>+32</td>
-      <td>81</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
-    </tr>
-    <tr>
-      <th>2</th>
-      <td><a href="https://en.wikipedia.org/wiki/Arsenal_F.C." title="Arsenal F.C.">Arsenal</a></td>
-      <td>38</td>
-      <td>20</td>
-      <td>11</td>
-      <td>7</td>
-      <td>65</td>
-      <td>36</td>
-      <td>+29</td>
-      <td>71</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
-    </tr>
-    <tr>
-      <th>3</th>
-      <td><a href="https://en.wikipedia.org/wiki/Tottenham_Hotspur_F.C." title="Tottenham Hotspur F.C.">Tottenham Hotspur</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>13</td>
-      <td>6</td>
-      <td>69</td>
-      <td>35</td>
-      <td>+34</td>
-      <td>70</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Group_stage" title="2016–17 UEFA Champions League">Champions League group stage</a></td>
-    </tr>
-    <tr class="is-selected">
-      <th>4</th>
-      <td><a href="https://en.wikipedia.org/wiki/Manchester_City_F.C." title="Manchester City F.C.">Manchester City</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>9</td>
-      <td>10</td>
-      <td>71</td>
-      <td>41</td>
-      <td>+30</td>
-      <td>66</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Champions_League#Play-off_round" title="2016–17 UEFA Champions League">Champions League play-off round</a></td>
-    </tr>
-    <tr>
-      <th>5</th>
-      <td><a href="https://en.wikipedia.org/wiki/Manchester_United_F.C." title="Manchester United F.C.">Manchester United</a></td>
-      <td>38</td>
-      <td>19</td>
-      <td>9</td>
-      <td>10</td>
-      <td>49</td>
-      <td>35</td>
-      <td>+14</td>
-      <td>66</td>
-      <td>Qualification for the <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_UEFA_Europa_League#Group_stage" title="2016–17 UEFA Europa League">Europa League group stage</a></td>
-    </tr>
+   
     
    
   </tbody>
 </table>
+</div>
         </div>
     );
 };
